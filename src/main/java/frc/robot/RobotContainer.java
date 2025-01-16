8f21f4ac3ac8e5357d57b224e61147bcc97f6a45
SLICE-8738 <fvhsrobotics@gmail.com>
2025-01-16 14:32:31 -0500

    Reimported Thrifty swerve branch into 2025

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,167 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.util.Set;
+
+//import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.DeferredCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+import frc.robot.commands.Drivetrain.*;
+import frc.robot.subsystems.*;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.drivetrain.RealSwerveModuleIO;
+import frc.robot.subsystems.drivetrain.SimSwerveModuleIO;
+import frc.robot.subsystems.drivetrain.SwerveModuleIO;
+import frc.robot.testing.routines.DrivetrainTest;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+
+  private final PS4Controller driverController = Button.controller1;
+  //private final GenericHID operatorController = Button.controller2;
+
+  // ==========================
+  // Subsystems
+  // ==========================
+
+  public final Drivetrain m_drivetrain;
+  public final LEDs m_leds;
+
+  public final AutoSelector m_autoSelector;
+  public final ShuffleboardData m_shuffleboardData;
+
+  // ==========================
+  // Commands
+  // ==========================
+
+  /* Drivetrain */
+  public final DriveCommand m_swerveDriveOpenLoop;
+  public final DriveCommand m_swerveDriveClosedLoop;
+  public final RunDutyCycleCommand m_setDrivePercentOutput;
+  public final ResetFieldOrientedHeading m_resetFieldOrientedHeading;
+  public final Command m_sysIDDriveRoutine;
+
+  /* Tests */
+  public final DrivetrainTest m_drivetrainTest;
+  
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+
+    // ==========================
+    // Subsystems
+    // ==========================
+
+    switch (Constants.ADVANTAGE_KIT_MODE) {
+        case REAL:
+          // Real robot, instantiate hardware IO implementations
+          m_drivetrain =
+            new Drivetrain(
+                new RealSwerveModuleIO(Constants.kDrivetrain.Mod0.CONSTANTS),
+                new RealSwerveModuleIO(Constants.kDrivetrain.Mod0.CONSTANTS),
+                new RealSwerveModuleIO(Constants.kDrivetrain.Mod0.CONSTANTS),
+                new RealSwerveModuleIO(Constants.kDrivetrain.Mod0.CONSTANTS));
+          break;
+        case SIM:
+          m_drivetrain =
+            new Drivetrain(
+              new SimSwerveModuleIO(),
+              new SimSwerveModuleIO(),
+              new SimSwerveModuleIO(),
+              new SimSwerveModuleIO());
+          break;
+        default:
+          m_drivetrain =
+            new Drivetrain(
+              new SwerveModuleIO() {},
+              new SwerveModuleIO() {},
+              new SwerveModuleIO() {},
+              new SwerveModuleIO() {});
+          break;
+    }
+
+    m_leds = new LEDs();
+
+    m_autoSelector = new AutoSelector(m_drivetrain);
+    m_shuffleboardData = new ShuffleboardData(m_drivetrain, m_autoSelector);
+
+    // ==========================
+    // Commands
+    // ==========================
+
+    /* Drivetrain */
+    m_swerveDriveOpenLoop = new DriveCommand(m_drivetrain, driverController, true);
+    m_swerveDriveClosedLoop = new DriveCommand(m_drivetrain, driverController,
+        false);
+    m_setDrivePercentOutput = new RunDutyCycleCommand(m_drivetrain, 0.10, 0);
+    m_resetFieldOrientedHeading = new ResetFieldOrientedHeading(m_drivetrain);
+    m_sysIDDriveRoutine = new DeferredCommand(m_drivetrain::getSysIDDriveRoutine, Set.of(m_drivetrain));
+
+    /* Tests */
+    m_drivetrainTest = new DrivetrainTest(m_drivetrain);
+
+    // Configure the trigger bindings
+    configureBindings();
+
+    m_drivetrain.setDefaultCommand(m_swerveDriveClosedLoop);
+
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+
+    // ================
+    // Driver Controls
+    // ================
+
+    /* Drivetrain */
+    Button.triangle1.onTrue(m_resetFieldOrientedHeading);
+    Button.controlPadLeft1.toggleOnTrue(m_sysIDDriveRoutine);
+
+    // ==================
+    // Operator Controls
+    // ==================
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+
+    return m_autoSelector.getAutoRoutine();
+
+  }
+
+}
---
+++ src/main/java/frc/robot/ShuffleboardData.java
@@ -0,0 +1,160 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.util.Map;
+
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+import frc.robot.subsystems.drivetrain.Drivetrain;
+
+/** Contains and runs all code needed to display all necessary information on Shuffleboard.*/
+public class ShuffleboardData {
+
+    private final ShuffleboardTab /*driverTab,*/ debugTab, swerveTab, autoTab;
+
+    public ShuffleboardData(Drivetrain drivetrain, AutoSelector autoSelector) {
+
+        //driverTab = Shuffleboard.getTab("Driver Tab");
+        debugTab = Shuffleboard.getTab("Debug Tab");
+        swerveTab = Shuffleboard.getTab("Swerve Tab");
+        autoTab = Shuffleboard.getTab("Autonomous");
+
+        //Displays the current velocity in meters per second of the left front swerve module on Shuffleboard
+        swerveTab.addDouble("Left Front Velocity", () -> drivetrain.getModuleStates()[0].speedMetersPerSecond).
+        withPosition(0, 0).
+        withSize(2, 1);
+        //Displays the current velocity in meters per second of the right front swerve module on Shuffleboard
+        swerveTab.addDouble("Right Front Velocity", () -> drivetrain.getModuleStates()[1].speedMetersPerSecond).
+        withPosition(7, 0).
+        withSize(2, 1);
+        //Displays the current velocity in meters per second of the right back swerve module on Shuffleboard
+        swerveTab.addDouble("Right Back Velocity", () -> drivetrain.getModuleStates()[2].speedMetersPerSecond).
+        withPosition(7, 3).
+        withSize(2, 1);
+        //Displays the current velocity in meters per second of the left back swerve module on Shuffleboard
+        swerveTab.addDouble("Left Back Velocity", () -> drivetrain.getModuleStates()[3].speedMetersPerSecond).
+        withPosition(0, 3).
+        withSize(2, 1);
+        
+        //Displays the current absolute encoder angle in degrees with no offset of the left front swerve module on Shuffleboard
+        swerveTab.addDouble("Left Front Absolute Angle", () -> drivetrain.getAbsoluteAngles()[0]).
+        withWidget(BuiltInWidgets.kDial).
+        withProperties(Map.of("Min", 0, "Max", 360)).
+        withPosition(0, 1).
+        withSize(2, 1);
+        //Displays the current absolute encoder angle in degrees with no offset of the left back swerve module on Shuffleboard
+        swerveTab.addDouble("Right Front Absolute Angle", () -> drivetrain.getAbsoluteAngles()[1]).
+        withWidget(BuiltInWidgets.kDial).
+        withProperties(Map.of("Min", 0,"Max", 360)).
+        withPosition(7, 1).
+        withSize(2, 1);
+        //Displays the current absolute encoder angle in degrees with no offset of the right front swerve module on Shuffleboard
+        swerveTab.addDouble("Right Back Absolute Angle", () -> drivetrain.getAbsoluteAngles()[2]).
+        withWidget(BuiltInWidgets.kDial).
+        withProperties(Map.of("Min", 0, "Max", 360)).
+        withPosition(7, 2).
+        withSize(2, 1);
+        //Displays the current absolute encoder angle in degrees with no offset of the right back swerve module on Shuffleboard
+        swerveTab.addDouble("Left Back Absolute Angle", () -> drivetrain.getAbsoluteAngles()[3]).
+        withWidget(BuiltInWidgets.kDial).
+        withProperties(Map.of("Min", 0, "Max", 360)).
+        withPosition(0, 2).
+        withSize(2, 1);
+        
+        //Displays the current integrated encoder angle in degrees of the left front swerve module on Shuffleboard
+        swerveTab.addDouble("Left Front Integrated Angle", () -> drivetrain.getModuleStates()[0].angle.getDegrees()).
+        withPosition(2, 0).
+        withSize(2, 1);
+        //Displays the current integrated encoder angle in degrees of the right front swerve module on Shuffleboard
+        swerveTab.addDouble("Right Front Integrated Angle", () -> drivetrain.getModuleStates()[1].angle.getDegrees()).
+        withPosition(5, 0).
+        withSize(2, 1);
+        //Displays the current integrated encoder angle in degrees of the right back swerve module on Shuffleboard
+        swerveTab.addDouble("Right Back Integrated Angle", () -> drivetrain.getModuleStates()[2].angle.getDegrees()).
+        withPosition(5, 3).
+        withSize(2, 1);
+        //Displays the current integrated encoder angle in degrees of the left back swerve module on Shuffleboard
+        swerveTab.addDouble("Left Back Integrated Angle", () -> drivetrain.getModuleStates()[3].angle.getDegrees()).
+        withPosition(2, 3).
+        withSize(2, 1);
+        
+        //Displays the current heading of the robot in degrees on Shuffleboard
+        debugTab.addDouble("Drivetrain Heading", () -> drivetrain.getHeading().getDegrees()).
+        withWidget(BuiltInWidgets.kDial).
+        withProperties(Map.of("Min", 0, "Max", 360)).
+        withPosition(0, 0).
+        withSize(2, 1);
+                
+        //Displays the current position of the robot on the field on Shuffleboard
+        debugTab.add(drivetrain.m_field2d).
+        withPosition(4, 2).
+        withSize(3, 2);
+
+        // //Adds a tuner for the drive motor PID gains to Shuffleboard
+        // ShuffleboardTuner.create(
+        //     (values) -> {
+          
+        //       drivetrain.setDrivePID(values[0], values[1], values[2]);
+          
+        //     },
+        //     new String[] {"kP", "kI", "kD"},
+        //     "Drive Motor PID");
+
+        // //Adds a tuner for the angle motor PID gains to Shuffleboard
+        // ShuffleboardTuner.create(
+        //     (values) -> {
+
+        //         drivetrain.setAnglePIDF(values[0], values[1], values[2], values[3]);
+
+        //     },
+        //     new String[] {"kP", "kI", "kD", "kFF"},
+        //     "Angle Motor PIDF");
+
+        // //Adds a tuner for the maximum velocities to Shuffleboard
+        // ShuffleboardTuner.create(
+        //     (values) -> {
+
+        //         drivetrain.maxLinearVelocity = values[0];
+        //         drivetrain.maxAngularVelocity = values[1];
+
+        //     },
+        //     new String[] {"Max Linear", "Max Angular"},
+        //     "Drivetrain Max Velocities");
+
+        debugTab.add("SysID Routine", drivetrain.sysIDChooser);
+
+        //Adds the sendable chooser for the desired autonomous mode onto Shuffleboard
+        autoTab.add("Auto Mode", autoSelector.modeChooser).withPosition(2, 0).withSize(2, 1);
+        //Adds the sendable chooser for the robot starting position onto Shuffleboard
+        autoTab.add("Starting Position", autoSelector.startingPositionChooser).withPosition(5, 0).withSize(2, 1);
+
+        //Displays the autonomous mode selected on the sendable chooser on Shuffleboard
+        autoTab.addString("Selected Auto Mode", autoSelector::getMode).
+        withPosition(2, 1).
+        withSize(2, 1);
+        //Displays the robot starting position selected on the sendable chooser on Shuffleboard
+        autoTab.addString("Selected Starting Position", autoSelector::getStartingPosition).
+        withPosition(5, 1).
+        withSize(2, 1);
+
+        //Displays the X offset of the robot from the inital pose of the selected autonomous routine on Shuffleboard
+        autoTab.addDouble("Initial Auto Pose X Offset", () -> autoSelector.initialAutoPoseXOffset).
+        withPosition(1, 2).
+        withSize(2, 1);
+        //Displays the Y offset of the robot from the inital pose of the selected autonomous routine on Shuffleboard
+        autoTab.addDouble("Initial Auto Pose Y Offset", () -> autoSelector.initialAutoPoseYOffset).
+        withPosition(6, 2).
+        withSize(2, 1);
+        //Displays the rotational offset of the robot from the inital pose of the selected autonomous routine on Shuffleboard
+        autoTab.addDouble("Initial Auto Pose Rotation Offset", () -> autoSelector.initialAutoPoseRotationOffset).
+        withPosition(3, 2).
+        withSize(3, 1);
+
+    }
+
+}
