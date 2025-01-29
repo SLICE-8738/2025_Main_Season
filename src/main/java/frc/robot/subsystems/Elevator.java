381d9a570e61fce503fd0c38a0067a0c9e799aa6
IcePenguin <130691092+IcePenguin1@users.noreply.github.com>
2025-01-28 20:37:55 -0500

    cleaned up and fixed Elevator

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -3,45 +3,19 @@ package frc.robot.subsystems;
 import frc.slicelibs.PositionalSubsystem;
 
 public class Elevator extends PositionalSubsystem{
-    private static final double LEVEL_ONE = 0;
-    private static final double LEVEL_TWO = 1;
-    private static final double LEVEL_THREE = 2;
-    private static final double LEVEL_FOUR = 4;
 
     private int[] ids;
-    private boolean[] inverted;
-    private double kP;
-    private double kI;
-    private double kD;
     private double positionConversionFactor;
-    private double velocityConversionFactor;
     
     public Elevator(int[] ids, boolean[] inverted, double kP, double kI, double kD, double positionConversionFactor, double velocityConversionFactor){
         super(ids, inverted, kP, kI, kD, positionConversionFactor, velocityConversionFactor);
         setEncoderPosition(0);
 
-        ids = this.ids;
-        inverted = this.inverted;
-        kP = this.kP;
-        kI = this.kI;
-        kD = this.kD;
-        positionConversionFactor = this.positionConversionFactor;
-        velocityConversionFactor = this.velocityConversionFactor;
+        this.ids = ids;
+        this.positionConversionFactor = positionConversionFactor;
     }
     
-    public void moveToLevelOne(){
-        setPosition(LEVEL_ONE * positionConversionFactor);
-    }
-
-    public void moveToLevelTwo(){
-        setPosition(LEVEL_TWO * positionConversionFactor);
-    }
-
-    public void moveToLevelThree(){
-        setPosition(LEVEL_THREE * positionConversionFactor);
-    }
-
-    public void moveToLevelFour(){
-        setPosition(LEVEL_FOUR * positionConversionFactor);
+    public void moveTo(double height){
+        setPosition(height * positionConversionFactor);
     }
 }
