af90eef56527eba82876a56b8500bd766871f2b3
IcePenguin1 <130691092+IcePenguin1@users.noreply.github.com>
2025-02-05 14:42:28 -0500

    created manual source intake rotation command. cleaned up other source intake code

--- src/main/java/frc/robot/commands/SourceIntake/ManualRotateSourceIntake.java
+++ src/main/java/frc/robot/commands/SourceIntake/ManualRotateSourceIntake.java
@@ -4,43 +4,52 @@
 
 package frc.robot.commands.SourceIntake;
 
-
+import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.SourceIntake;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class RotateSourceIntake extends Command {
-  /** Creates a new RotateSourceIntake. */
+public class ManualRotateSourceIntake extends Command {
   private final SourceIntake m_sourceIntake;
-  private final double m_threshold;
-  private final double m_degrees;
+  private final PS4Controller m_controller;
+  private double axis;
 
-  public RotateSourceIntake(SourceIntake sourceIntake, double threshold, double degrees) {
+  /** Creates a new ManualRotateSourceIntake. */
+  public ManualRotateSourceIntake(SourceIntake intake, PS4Controller controller) {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(sourceIntake);
+    addRequirements(intake);
 
-    m_sourceIntake = sourceIntake;
-    m_threshold = threshold;
-    m_degrees = degrees;
+    m_sourceIntake = intake;
+    m_controller = controller;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    m_sourceIntake.setPosition(m_degrees);
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    axis = m_controller.getRightX();
+    m_sourceIntake.set(axis);
+    if(m_sourceIntake.getPosition() <= m_sourceIntake.getDefaultPosition() && axis < 0){
+      m_sourceIntake.set(0);
+    }
+    if(m_sourceIntake.getPosition() >= 180 && axis > 0){
+      m_sourceIntake.set(0);
+    }
+    
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_sourceIntake.set(0);
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return m_sourceIntake.atTarget(m_threshold);
+    return false;
   }
 }
