bf91021b8f5fb7af31c8fa8404e756521d489640
QueenRominaMartin <goldenscar87@gmail.com>
2025-02-10 14:32:13 -0500

    end effector subsystem completed pending some changes

--- src/main/java/frc/robot/subsystems/EndEffector.java
+++ src/main/java/frc/robot/subsystems/EndEffector.java
@@ -6,17 +6,36 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.hal.DIOJNI;
+import edu.wpi.first.wpilibj.simulation.DIOSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class EndEffector extends SubsystemBase {
   private TalonFX rotationMotor;
   private TalonFX placementMotor;
+  //TODO rename maybe idk
+  /* HOW INDEXING CORAL WORKS
+   * Coral begins indexing into the end effector and begins to trip the back sensor
+   * Coral continues being indexed and trips the middle sensor
+   * Coral continues to index and trips the front sensor
+   * Then untrips the back sensor
+   * The untrips the middle sensor which signals that the coral has indexed too far
+   * The end effector spins opposite to bring coral back in and retrip middle sensor
+   * Coral successfully indexed
+   */
+  private static DIOJNI frontSensor; 
+  private static DIOJNI backSensor; 
+  private static DIOJNI middleSensor; 
 
   /** Creates a new EndEffector. */
   public EndEffector() {
     rotationMotor = new TalonFX(Constants.kEndEffector.ROTATION_MOTOR_ID);
     placementMotor = new TalonFX(Constants.kEndEffector.PLACEMENT_MOTOR_ID);
+    //TODO enter parameters
+    frontSensor.initializeDIOPort(0, false);
+    backSensor.initializeDIOPort(0, false);
+    middleSensor.initializeDIOPort(0, false);
   }
 
   public void setRotationMotorSpeed() {
@@ -33,6 +52,10 @@ public class EndEffector extends SubsystemBase {
 
   public void stopPlacementMotor() {
     rotationMotor.set(0);
+  }
+
+  public Boolean getSensorValues() {
+    frontSensor.getDIO(0);
   }
 
   @Override
