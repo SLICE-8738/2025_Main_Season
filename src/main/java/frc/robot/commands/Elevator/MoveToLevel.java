bf2bde35ae660a1b15db09c1509af28070f4743e
IcePenguin1 <130691092+IcePenguin1@users.noreply.github.com>
2025-02-13 15:15:13 -0500

    Changed Elevator to extend TalonFXPositionalSubsystem

--- src/main/java/frc/robot/commands/Elevator/MoveToLevel.java
+++ src/main/java/frc/robot/commands/Elevator/MoveToLevel.java
@@ -3,46 +3,45 @@ package frc.robot.commands.Elevator;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Elevator;
 
-public class MoveToLevel extends Command{
+public class MoveToLevel extends Command {
     private final Elevator m_elevator;
     private final double m_level;
     private final double m_threshold;
 
-    ///true is upwards movement. false is downwards movement.
+    /// true is upwards movement. false is downwards movement.
     private boolean movementDirection = false;
-    
 
-    public MoveToLevel(Elevator elevator,double level, double threshold){
+    public MoveToLevel(Elevator elevator, double level, double threshold) {
         addRequirements(elevator);
-        
+
         m_elevator = elevator;
         m_level = level;
         m_threshold = threshold;
     }
 
-    public void initialize(){
-        if (m_level > m_elevator.getPosition()){
+    public void initialize() {
+        if (m_level > m_elevator.getPosition()[0]) {
             movementDirection = true;
-        } 
+        }
     }
 
-    public void execute(){
+    public void execute() {
         m_elevator.moveTo(m_level);
     }
 
-    public void end(){
+    public void end() {
 
     }
 
-    public boolean isFinished(){
+    public boolean isFinished() {
         boolean finished = false;
-        if(m_elevator.atTarget(m_threshold)){
+        if (m_elevator.atTarget(m_threshold)) {
             finished = true;
         }
-        if(m_elevator.isAtTop() && movementDirection){
+        if (m_elevator.isAtTop() && movementDirection) {
             finished = true;
         }
-        if(m_elevator.isAtBottom() && !movementDirection){
+        if (m_elevator.isAtBottom() && !movementDirection) {
             finished = true;
         }
 
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,30 +1,34 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import frc.slicelibs.PositionalSubsystem;
+import com.ctre.phoenix6.signals.GravityTypeValue;
 
-public class Elevator extends PositionalSubsystem{
+import edu.wpi.first.wpilibj.DigitalInput;
+import frc.slicelibs.TalonFXPositionalSubsytem;
+
+public class Elevator extends TalonFXPositionalSubsytem {
 
     private DigitalInput bottomLimitSwitch;
     private DigitalInput topLimitSwitch;
-    
-    public Elevator(int[] ids, boolean[] inverted, double kP, double kI, double kD, double positionConversionFactor, double velocityConversionFactor, DigitalInput bottomLimitSwitch, DigitalInput topLimitSwitch){
-        super(ids, inverted, kP, kI, kD, positionConversionFactor, velocityConversionFactor);
+
+    public Elevator(int[] ids, boolean[] inverted, double kP, double kI, double kD, double positionConversionFactor,
+            double velocityConversionFactor, DigitalInput bottomLimitSwitch, DigitalInput topLimitSwitch) {
+        super(ids, inverted, kP, kI, kD, GravityTypeValue.Elevator_Static, positionConversionFactor,
+                velocityConversionFactor);
         setEncoderPosition(0);
 
         this.bottomLimitSwitch = bottomLimitSwitch;
         this.topLimitSwitch = topLimitSwitch;
     }
-    
-    public void moveTo(double height){
+
+    public void moveTo(double height) {
         setPosition(height);
     }
 
-    public boolean isAtBottom(){
+    public boolean isAtBottom() {
         return bottomLimitSwitch.get();
     }
 
-    public boolean isAtTop(){
+    public boolean isAtTop() {
         return topLimitSwitch.get();
     }
 }
