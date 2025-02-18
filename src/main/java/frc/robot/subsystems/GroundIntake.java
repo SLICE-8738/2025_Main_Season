9d9fd67ae7a8f00a24d7764e7b38bac57b8f32af
Yug-Sarin <321sarinyug@gmail.com>
2025-02-18 15:44:08 -0500

    started actually working on Ground Intake

--- src/main/java/frc/robot/subsystems/GroundIntake.java
+++ src/main/java/frc/robot/subsystems/GroundIntake.java
@@ -4,19 +4,35 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class GroundIntake extends SubsystemBase {
+
   private TalonFX groundMotor;
-  private Encoder groundEncoder;
+  private double positionConversionFactor;
+  private double velocityConversionFactor;
+  private double positionTargetReference;
+  private double velocityTargetReference;
 
   /** Creates a new GroundIntake. */
-  public GroundIntake() {
+  public GroundIntake(double kP, double kI, double kD, double PCF, double VCF) {
+    //TalonFX and TalonFX Configuration
     groundMotor = new TalonFX(0); // TODO - Change device ID.
-    groundEncoder = new Encoder(0, 0); // TODO - Change channels.
+    TalonFXConfiguration configuration = new TalonFXConfiguration();
+    configuration.Slot0.kP = kP;
+    configuration.Slot0.kI = kI;
+    configuration.Slot0.kD = kD;
+    groundMotor.getConfigurator().apply(configuration);
+
+    // Other needed in Constructor
+    positionConversionFactor = PCF;
+    velocityConversionFactor = VCF;
   }
 
   /**
@@ -24,6 +40,12 @@ public class GroundIntake extends SubsystemBase {
    */
   public void moveGroundIntake(double speed) {
     groundMotor.set(speed);
+  }
+
+  public void setVelocity(double velocity){
+    VelocityVoltage velocityRequest = new VelocityVoltage(0).withSlot(0);
+    groundMotor.setControl(velocityRequest.withVelocity(velocity * velocityConversionFactor));
+    velocityTargetReference = velocity;
   }
 
   @Override
