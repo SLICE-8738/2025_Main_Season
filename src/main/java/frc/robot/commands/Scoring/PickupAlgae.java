8ee828d0d39a91b32dc29b1e48ebb41f913ae2e0
IcePenguin <130691092+IcePenguin1@users.noreply.github.com>
2025-02-27 00:18:12 -0500

    Added PickupAlgae command

--- src/main/java/frc/robot/commands/Scoring/PickupAlgae.java
+++ src/main/java/frc/robot/commands/Scoring/PickupAlgae.java
@@ -2,19 +2,24 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.Scoring;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.commands.IntakeAlgae;
+import frc.robot.commands.Elevator.ElevatorToAlgae;
+import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.EndEffector;
+import frc.robot.Constants.kElevator.Level;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class IntakeAlgae extends SequentialCommandGroup {
-  /** Creates a new IntakeAlgae. */
-  public IntakeAlgae(EndEffector endEffector) {
+public class PickupAlgae extends ParallelCommandGroup {
+  /** Creates a new PickupAlgae. */
+  public PickupAlgae(Elevator elevator, Level level, EndEffector endEffector) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new PrepareEndEffector(endEffector, 20), new MotorIntakeAlgae(endEffector));
+
+    addCommands(new ElevatorToAlgae(elevator, level), new IntakeAlgae(endEffector));
   }
 }
